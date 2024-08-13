d79abf929110ec4712325f8696d981dfaa71cde4
shivanshudevtelstra <Shivanshu.Dev@team.telstra.com>
2024-08-13 12:32:38 +0530

    hello ji

--- payroll.js
+++ payroll.js
@@ -51,4 +51,4 @@ employeeList.addEmployee("Diana Prince", "Human Resources", 68000.00);
 employeeList.addEmployee("Evan Davis", "Finance", 72000.00);
 
 employeeList.displayEmployees();
-console.log("everything works fine");
+console.log("everything works");
